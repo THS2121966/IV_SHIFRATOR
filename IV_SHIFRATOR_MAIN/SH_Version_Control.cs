2b28a0a79483fdce17c275ef12f8461695ef2d2c
Ivan Suvorov <avatar2329@gmail.com>
2022-01-15 18:50:26 +0600

    Update SH_Version_Control.cs

--- IV_SHIFRATOR_MAIN/SH_Version_Control.cs
+++ IV_SHIFRATOR_MAIN/SH_Version_Control.cs
@@ -13,7 +13,7 @@ namespace IV_SHIFRATOR_MAIN
 #if SHIFRATOR_VERSION_015
         private static readonly float sh_programm_ver = 0.15f;
 #endif
-        private static readonly string sh_programm_ver_text = "'Current programm ver' =  '"+sh_programm_ver+"'";
+        private static readonly string sh_programm_ver_text = "'Current programm ver' = '"+sh_programm_ver+"'";
 
         private bool sh_ver_realised = false;
         private float sh_version_sended;
@@ -61,19 +61,40 @@ namespace IV_SHIFRATOR_MAIN
             {
                 StreamReader sh_check_ver_from_file = new StreamReader(sh_path);
                 var sh_sended_ver = sh_check_ver_from_file.ReadLine();
+                if (sh_sended_ver == null)
+                    sh_sended_ver = sh_programm_ver_text;
                 if (sh_sended_ver == sh_programm_ver_text)
                 {
+                    bool sh_recreate_ver_to_file = false;
+                    if(sh_check_ver_from_file.ReadLine() == null)
+                    {
+                        sh_recreate_ver_to_file = true;
+                    }
                     sh_check_ver_from_file.Close();
+                    if(sh_recreate_ver_to_file)
+                    {
+                        StreamWriter sh_rect = new StreamWriter(sh_path);
+                        sh_rect.WriteLine(sh_programm_ver_text);
+                        sh_rect.Close();
+                    }
                     sh_ver_realised = true;
                     sh_version_sended = sh_programm_ver;
                 }
-                else
+                else if(sh_sended_ver.Substring(1,22) != sh_programm_ver_text.Substring(1,22) || sh_sended_ver.Substring(22, 4) != sh_programm_ver_text.Substring(22, 4) 
+                    && sh_sended_ver != String.Empty)
                 {
                     sh_check_ver_from_file.Close();
                     MessageBox.Show("Version not readed from selected file!!! Tell a programmer!!! String = " + sh_sended_ver + ".",
                         "Shifrator Version Control", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     sh_ver_realised = false;
                     SH_Loading_Window.sh_loading_core.Close();
+                }
+                else if(float.TryParse(sh_sended_ver.Substring(27, 3), out float sh_new_ver_include))
+                {
+                    sh_check_ver_from_file.Close();
+                    sh_version_sended = sh_new_ver_include;
+                    MessageBox.Show("Current sended version = "+sh_version_sended+".", "Shifrator Version Control", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    sh_ver_realised = true;
                 }
             }
         }
