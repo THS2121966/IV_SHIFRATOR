ae11e7f81a9e53b75ce722072bfbde9f258c7ae6
Ivan Suvorov <avatar2329@gmail.com>
2022-02-01 22:48:14 +0600

    Update github_publish_archive_create.bat

--- IV_SHIFRATOR_MAIN/github_publish_archive_create.bat
+++ IV_SHIFRATOR_MAIN/github_publish_archive_create.bat
@@ -5,6 +5,9 @@ echo IV_CREATE_FOLDERS_COMPLETE!!!
 echo IV_COPING_MAIN_PROGRAMM_AND_INCLUDES...
 copy .\IV_Shifrator.exe .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
 copy .\Siticone.Desktop.UI.dll .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
+copy .\Skybound.Gecko.dll .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
+copy .\Skybound.Gecko.xml .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
+xcopy /s .\thirdparty .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator\thirdparty
 echo IV_COPIED_MAIN_PROGRAMM_AND_INCLUDES!!!
 echo IV_ARCHIVE_MAIN_FOLDER_FOR_RELEASING_THAT...
 "C:\Program Files\WinRAR\WinRAR.exe" a -ag -m5 ".\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator.rar" ".\IV_SHIFRATOR_MAIN_GIT_Release"
