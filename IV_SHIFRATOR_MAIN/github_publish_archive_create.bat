607dcb7661eb502e2079b1fe03efc19d1101edc3
Ivan Suvorov <avatar2329@gmail.com>
2022-02-02 18:15:31 +0600

    Update github_publish_archive_create.bat

--- IV_SHIFRATOR_MAIN/github_publish_archive_create.bat
+++ IV_SHIFRATOR_MAIN/github_publish_archive_create.bat
@@ -5,6 +5,7 @@ MD .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator\thirdparty
 echo IV_CREATE_FOLDERS_COMPLETE!!!
 echo IV_COPING_MAIN_PROGRAMM_AND_INCLUDES...
 copy .\IV_Shifrator.exe .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
+copy .\IVConsole.dll .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
 copy .\Siticone.Desktop.UI.dll .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
 copy .\Skybound.Gecko.dll .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
 copy .\Skybound.Gecko.xml .\IV_SHIFRATOR_MAIN_GIT_Release\IV_Shifrator
