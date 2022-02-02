d74fc2ed46f96ff2b8d8d7a074d95cf8f956e4d3
Ivan Suvorov <avatar2329@gmail.com>
2022-02-02 16:42:46 +0600

    [THS] Add Console Window Base File.

---
+++ IV_Console/IV_Console_Window.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IV_Console
+{
+    public partial class IV_Console_Window : Form
+    {
+        public IV_Console_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
